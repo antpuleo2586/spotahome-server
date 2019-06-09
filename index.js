55bbfffe50ffb9f1aa6ea822000709516c2c5393
Ant <antonio.puleo@uswitch.com>
2019-06-09 17:10:25 +0100

    enabling playground in prod

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { ApolloServer, gql } = require('apollo-server')
 const typeDefs = require('./graphql/typeDefs')
 const resolvers = require('./graphql/resolvers')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true })
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
     console.log(`🚀  Server ready at ${url}`)
